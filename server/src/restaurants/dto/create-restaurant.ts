ddc33ca4ed4aaa6b8a0c518e71e32da8bea752b3
chspower1 <chspower1@gmail.com>
2023-03-01 22:13:43 +0900

    class validation 체크하는 법

--- server/src/restaurants/dto/create-restaurant.ts
+++ server/src/restaurants/dto/create-restaurant.ts
@@ -1,13 +1,18 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString, IsBoolean, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(() => String)
+  @IsString()
+  @Length(2, 6)
   name: string;
 
   @Field(() => String)
+  @IsString()
   location: string;
 
   @Field(() => Boolean)
+  @IsBoolean()
   isVegan: boolean;
 }
